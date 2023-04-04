72279d1956e912248ac904529643389fc67bfaab
leandrofiore3 <leandrofiore3@gmail.com>
2023-04-04 17:28:17 -0300

    proyecto calculadora

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+
+function calculate() {
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
+  let operator = document.querySelector('input[name="operator"]:checked').value;
+  let result;
+
+  if (operator === "Sumar") {
+    result = num1 + num2;
+  } else if (operator === "Restar") {
+    result = num1 - num2;
+  } else if (operator === "Multiplicar") {
+    result = num1 * num2;
+  } else if (operator === "Dividir") {
+    result = num1 / num2;
+  }
+
+  alert("El resultado es: " + result);
+  
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
+}
